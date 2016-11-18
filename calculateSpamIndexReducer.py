99b26c9efd22ff794ab85ffae1fe099b47e9f390
jyotsnaparyani <jyotsna.paryani@okstate.edu>
2016-11-18 17:07:47 -0600

    Create calculateSpamIndexReducer.py

---
+++ calculateSpamIndexReducer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+def readFileandReturnAnArray(fileName, readMode, isLower):
+
+    with open(fileName, readMode) as readHandle:
+        for line in readHandle.readlines():
+            lineRead = line
+            if isLower:
+                lineRead = lineRead.lower()
+            count = (lineRead.strip().lstrip())
+    readHandle.close()
+    return count
+
+totalTweetCount = readFileandReturnAnArray("tweetCount", "r", True)
+totalTweetCount = float(totalTweetCount)
+
+geo_score = {}
+
+for line in sys.stdin:
+    line = line.strip()
+    if line!="" and line is not None:
+        topicId, location = line.split('\t')
+        if topicId in geo_score:
+                existingValues = geo_score.get(topicId)
+                if location is not "" and location != None and location not in existingValues:
+                        geo_score[topicId].append(location)
+        else:
+                geo_score[topicId] = []
+                if location is not "" and location != None:
+                        geo_score[topicId].append(location)
+
+
+for topic in geo_score.keys():
+    list_of_values = geo_score[topic]
+    length = len(list_of_values)
+    spamIndex = 1.00/length
+    final_score = float(spamIndex)/totalTweetCount
+    print '%s\t%s'% (topic, final_score)
